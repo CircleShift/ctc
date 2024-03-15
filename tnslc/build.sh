f80bb4fd79f27210b606966c421ff3104ad0c959
Kyle Gunger <kgunger12@gmail.com>
2024-03-15 17:28:48 -0400

    Build script

--- tnslc/build.sh
+++ tnslc/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-../ctc main.tnsl tnslc.asm
-nasm -f elf64 -o tnslc.o tnslc.asm
-gcc -o tnslc tnslc.o
+mkdir -p out
+../ctc main.tnsl out/tnslc.asm
+nasm -f elf64 -o out/tnslc.o out/tnslc.asm
+gcc -o out/tnslc out/tnslc.o
 # rm tnslc.asm tnslc.o
 
