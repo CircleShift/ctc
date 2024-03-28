670a9729fdd425c1b920f8af1c582dfeceb9e721
Kyle Gunger <kgunger12@gmail.com>
2024-03-28 15:18:15 -0400

    Change run script to test all

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-mkdir -p ./out/artifact
-filename=$1
-filename="${filename%.*}"
-../ctc $1 out/artifact/$filename.asm
-nasm -f elf64 -o ./out/artifact/$filename.o ./out/artifact/$filename.asm
-gcc -o ./out/$filename ./out/artifact/$filename.o
+make
+
+for i in out/*.out; do
+	./$i
+	if [[ $? -ne 69 ]]; then
+		echo "[FAILED] $i"
+	else
+		echo "[  OK  ] $i"
+	fi
+done
 
